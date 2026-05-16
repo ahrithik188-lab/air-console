9f123c3aa89266ef37e8f4c49039a0070d483e89
ahrithik188-lab <ahrithik188@gmail.com>
2026-05-16 15:03:29 +0530

    server.js

--- server.js
+++ server.js
@@ -1,76 +1,67 @@
 const express = require("express");
-const http = require("http");
-const { Server } = require("socket.io");
 
 const app = express();
 
-const server = http.createServer(app);
+const http = require("http").createServer(app);
 
-const io = new Server(server);
+const io = require("socket.io")(http);
 
-app.use(express.static("public"));
-
-let players = {};
+app.use(express.static(__dirname));
 
 io.on("connection", (socket) => {
 
-    console.log("Connected:", socket.id);
+    console.log("User Connected");
+
+    // JOIN ROOM
 
     socket.on("join-room", (room) => {
 
         socket.join(room);
 
-        players[socket.id] = room;
+        const clients =
+        io.sockets.adapter.rooms.get(room);
 
         const count =
-            io.sockets.adapter.rooms.get(room)?.size || 0;
+        clients ? clients.size : 0;
 
-        io.to(room).emit("player-count", count);
+        io.to(room).emit(
+            "player-count",
+            count
+        );
 
-        console.log(socket.id + " joined " + room);
+        console.log(
+            "Joined Room:",
+            room
+        );
 
     });
 
-    socket.on("controller-move", (direction) => {
+    // CONTROLLER INPUT
 
-        const room = players[socket.id];
+    socket.on(
+        "controller-move",
+        (data) => {
 
-        if(room){
-
-            socket.to(room).emit(
+            io.to(data.room).emit(
                 "controller-update",
-                direction
+                data.direction
             );
 
         }
-
-    });
+    );
 
     socket.on("disconnect", () => {
 
-        const room = players[socket.id];
-
-        delete players[socket.id];
-
-        if(room){
-
-            const count =
-                io.sockets.adapter.rooms.get(room)?.size || 0;
-
-            io.to(room).emit("player-count", count);
-
-        }
-
-        console.log("Disconnected");
+        console.log("User Disconnected");
 
     });
 
 });
 
-const PORT = process.env.PORT || 3000;
+http.listen(3000, () => {
 
-server.listen(PORT, () => {
-
-    console.log("Server running");
+    console.log(
+        "Server running on port 3000"
+    );
 
 });
