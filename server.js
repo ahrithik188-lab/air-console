b6d7d759fa17dac3b48dd08984e0cda79b6c4b8c
ahrithik188-lab <ahrithik188@gmail.com>
2026-05-16 10:29:21 +0530

    server.js

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const http = require("http");
 const { Server } = require("socket.io");
 
 const app = express();
+
 const server = http.createServer(app);
+
 const io = new Server(server);
 
 app.use(express.static("public"));
@@ -11,39 +13,64 @@ app.use(express.static("public"));
 let players = {};
 
 io.on("connection", (socket) => {
-    console.log("Player connected:", socket.id);
+
+    console.log("Connected:", socket.id);
 
     socket.on("join-room", (room) => {
+
         socket.join(room);
 
         players[socket.id] = room;
 
-        io.to(room).emit("player-count", io.sockets.adapter.rooms.get(room)?.size || 0);
+        const count =
+            io.sockets.adapter.rooms.get(room)?.size || 0;
 
-        console.log(`Player joined room ${room}`);
+        io.to(room).emit("player-count", count);
+
+        console.log(socket.id + " joined " + room);
+
     });
 
-    socket.on("controller-move", (data) => {
+    socket.on("controller-move", (direction) => {
+
         const room = players[socket.id];
 
-        if (room) {
-            socket.to(room).emit("controller-update", data);
+        if(room){
+
+            socket.to(room).emit(
+                "controller-update",
+                direction
+            );
+
         }
+
     });
 
     socket.on("disconnect", () => {
+
         const room = players[socket.id];
 
         delete players[socket.id];
 
-        if (room) {
-            io.to(room).emit("player-count", io.sockets.adapter.rooms.get(room)?.size || 0);
+        if(room){
+
+            const count =
+                io.sockets.adapter.rooms.get(room)?.size || 0;
+
+            io.to(room).emit("player-count", count);
+
         }
 
-        console.log("Disconnected:", socket.id);
+        console.log("Disconnected");
+
     });
+
 });
 
-server.listen(3000, () => {
-    console.log("Server running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+
+    console.log("Server running");
+
 });
